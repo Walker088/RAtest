3f71a0ee5c69c2a64329901b2ad662ccb4fb4a81
Hector Chen <walker088@walker088.tw>
2019-05-07 23:27:54 +0800

    fix output

--- test.py
+++ test.py
@@ -21,21 +21,21 @@ def query(id, url=url_his, api=api_his):
 def writeRes(lrLst, trLst):
     with open('latency.csv', 'w', newline='') as lrCsv:
         writer = csv.writer(lrCsv)
-        writer.writerow('number_of_requests', 'latency')
+        writer.writerow(['number_of_requests', 'latency'])
         for nor in range(len(lrLst)):
             writer.writerow([10**(nor+1), lrLst[nor]])
         for nt in range(len(trLst)):
-            writer.writerow(nt+1, trTb[nt])
+            writer.writerow([nt+1, trLst[nt]])
 
 def printRes(lrLst, trLst):
     lrTb = PrettyTable()
-    lrTb.field_names = ['number_of_requests', 'latency']
+    lrTb.field_names = ['number_of_requests', 'latency(ms/r)']
     trTb = PrettyTable()
-    trTb.field_names = ['number_of_threads', 'throughput']
+    trTb.field_names = ['number_of_threads', 'throughput(r/s)']
     for nor in range(len(lrLst)):
-        lrTb.add_row([10**(nor+1), lrLst[nor]])
+        lrTb.add_row([10**(nor+1), int(lrLst[nor]*1000)])
     for nt in range(len(trLst)):
-        trTb.add_row(nt+1, trTb[nt])
+        trTb.add_row([nt+1, int(trLst[nt])])
     print(lrTb, trTb)
 
 def TputTest(url, api):
@@ -68,12 +68,14 @@ def LatencyTest(url, api):
     return lrLst
 
 if __name__ == '__main__':
-    lr = LatencyTest(url_his, api_his)
-    tr = TputTest(url_his, api_his)
-    print('LatencyTest:', lr, 'TputTest', tr)
+    #lr = LatencyTest(url_his, api_his)
+    #tr = TputTest(url_his, api_his)
+    lr = [0.3029536008834839, 0.2214457130432129, 0.1843657989501953, 0.18939784162044526]
+    tr = [3.983674122527586, 6.866074402356782, 13.971673269752504, 9.58865809346394, 13.990261829086482, 30.513835682900563, 15.752908931499869, 18.684565604867185, 55.515026023594956, 35.01826261225654]
+    printRes(lr, tr)
     writeRes(lr, tr)
 
     #lr = LatencyTest(url_saa, api_saa)
     #tr = TputTest(url_saa, api_saa)
     #print('LatencyTest:', lr, 'TputTest', tr)
-    writeRes(lr, tr)
+    #writeRes(lr, tr)
